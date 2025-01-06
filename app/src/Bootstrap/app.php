bef0ecfafee7a07b0c621dc12abf09761d44d616
Johnata Matheus <johnata@paytour.com.br>
2025-01-06 14:27:03 -0300

    feat: add cors and middlewares for app start

--- app/src/Bootstrap/app.php
+++ app/src/Bootstrap/app.php
@@ -2,6 +2,8 @@
 
 require __DIR__ . '/../../../vendor/autoload.php';
 
+use App\Http\Middlewares\CorsMiddleware;
+use App\Http\Middlewares\GlobalExceptionHandler;
 use App\Repositories\Interfaces\TaskRepositoryInterface;
 use App\Repositories\TaskRepository;
 use DI\ContainerBuilder;
@@ -10,6 +12,9 @@ use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../../../');
 $dotenv->load(); 
+
+$cors = new CorsMiddleware();
+$cors->handle();
 
 $containerBuilder = new ContainerBuilder();
 $containerBuilder->addDefinitions([
@@ -21,6 +26,9 @@ AppFactory::setContainer($container);
 
 // criação do Slim
 $app = AppFactory::create();
+
+$exceptionMiddleware = $app->addErrorMiddleware(true, true, true);
+$exceptionMiddleware->setDefaultErrorHandler(new GlobalExceptionHandler());
 
 require __DIR__ . '/../Http/routes/routes.php';
 

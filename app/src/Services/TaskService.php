b3dade7a6d982a3d06f8c11db4d474d88022ea96
Johnata Matheus <johnata@paytour.com.br>
2025-01-06 14:30:38 -0300

    fix: adjust task service

--- app/src/Services/TaskService.php
+++ app/src/Services/TaskService.php
@@ -2,7 +2,7 @@
 
 namespace App\Services;
 
-use App\Exceptions\TaskServiceException;
+use App\Exceptions\TaskNotFoundException;
 use App\Repositories\Interfaces\TaskRepositoryInterface;
 use App\Models\TaskModel;
 
@@ -17,75 +17,65 @@ class TaskService
 
   public function fetchAllTasks(): array
   {
-    try {
-      return $this->repository->findAll();
-    } catch (\Exception $e) {
-      throw new TaskServiceException("Erro ao buscar as tarefas: " . $e->getMessage());
+    $tasks = $this->repository->findAll();
+
+    if(!$tasks) {
+      throw new TaskNotFoundException('Nenhuma tarefa encontrada.');
     }
+
+    return $tasks;
   }
 
   public function fetchTaskById(int $id): array
   {
-    try {
-      return $this->repository->findById($id);
-    } catch (\Exception $e) {
-      throw new TaskServiceException("Erro ao buscar tarefa com ID: {$id} " . $e->getMessage());
+    $task = $this->repository->findById($id);
+    
+    if(!$task) {
+      throw new TaskNotFoundException();
     }
+
+    return $task;
   }
 
   public function createTask(array $data): bool
   {
-    try {
-      $task = new TaskModel(
-        title: $data['title'],
-        description: $data['description'],
-        status: $data['status'] ?? 'pending',
-      );
+    $task = new TaskModel(
+      title: $data['title'],
+      description: $data['description'],
+      status: $data['status'] ?? 'pending',
+    );
 
-      return $this->repository->save($task);
-    } catch (\Exception $e) {
-      throw new TaskServiceException("Erro ao criar a tarefa: " . $e->getMessage());
-    }
+    return $this->repository->save($task);
   }
 
   public function updateTask(int $id, array $data): bool
   {
-    try {
-      $existingTask = $this->repository->findById($id);
+    $task = $this->repository->findById($id);
 
-      if(!$existingTask) {
-        return false;
-      }
-
-      $updatedTask = new TaskModel(
-        id: $id,
-        title: $data['title'] ?? $existingTask['title'],
-        description: $data['description'] ?? $existingTask['description'],
-        status: $data['status'] ?? $existingTask['status'],
-        created_at: $existingTask['created_at'],
-        updated_at: date('Y-m-d H:i:s'),
-      );
-
-      return $this->repository->update($updatedTask);
-
-    } catch (\Exception $e) {
-      throw new TaskServiceException("Erro ao atualizar a tarefa com ID {$id} ". $e->getMessage());
+    if(!$task) {
+      throw new TaskNotFoundException();
     }
+
+    $updatedTask = new TaskModel(
+      id: $id,
+      title: $data['title'] ?? $task['title'],
+      description: $data['description'] ?? $task['description'],
+      status: $data['status'] ?? $task['status'],
+      created_at: $task['created_at'],
+      updated_at: date('Y-m-d H:i:s'),
+    );
+
+    return $this->repository->update($updatedTask);
   }
 
   public function deleteTask(int $id): bool
   {
-    try { 
-      $existingTask = $this->repository->findById($id);
-      
-      if(!$existingTask) {
-        return false;
-      }
+    $task = $this->repository->findById($id);
 
-      return $this->repository->delete($id);
-      
-    } catch (\Exception $e) {
-      throw new TaskServiceException("Erro ao deletar tarefa com ID {$id}" . $e->getMessage());
+    if(!$task) {
+      throw new TaskNotFoundException();
     }
+
+    return $this->repository->delete($id);  
   }
 }
